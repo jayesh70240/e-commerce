86fd82c33d37ef73d81b99aa1e5a4ed10f100cd9
Jayesh <jayeshharda@gmail.com>
2024-01-25 13:58:20 +0530

    andoriod i phone container

--- src/components/Navbar/Services/Services.jsx
+++ src/components/Navbar/Services/Services.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Services.css";
-import ios from "./iOS-deve-white.png";
+import ios from "./iOS-deve.png";
+import iosmini from "./iOS-deve-white.png"
+import android from "./android-deve.png"
 
 function Services() {
+  const [hover, setHover] = useState(null);
+
+  const handleHoverEffect = (link) => {
+    setHover(link);
+  };
+
+  const handleHoverLeave = () => {
+    setHover(null);
+  };
+
   return (
     <>
       <div className="main-container">
@@ -14,20 +26,95 @@ function Services() {
           </p>
         </div>
         <div className="service-content">
-          <div className="col-md-6">
-            <img src={ios} alt="ios" />
+          <div
+            className="col-md-6"
+            onMouseEnter={() => handleHoverEffect("iphone")}
+            onMouseLeave={handleHoverLeave}
+          >
+            {hover === "iphone" ? (
+              <div className="hoverContent">
+              <img className="imgmini" src={iosmini} alt="iosmini" />
+              <h6>Iphone App Development</h6>
+              <p>
+                We make use of modern programming languages like Swift and
+                Objective-C to offer high-end iPhone app development services.
+                With proven technologies and approaches, we have developed more
+                than 150 iPhone apps of various categories.
+              </p>
+              </div>
+            ) : (
+              <div className="img-with-title">
+                <img src={ios} alt="ios" />
+                <h4 className="d-flex">iPhone App Development</h4>
+              </div>
+            )}
+          </div>
+          <div
+            className="col-md-6"
+            onMouseEnter={() => handleHoverEffect("android")}
+            onMouseLeave={handleHoverLeave}
+          >
+            {hover === "android" ? (
+              <div className="hoverContent">
+              <img className="imgmini" src={android} alt="androidmini" />
+              <h6>Android App Development</h6>
+              <p>
+                We have successfully developed 200+ Android apps of various categories, including on-demand, health & fitness and other top apps categories. Using Java and Kotlin, we build Android apps from scratch for Android devices to smartwatches and smart TVs.
+              </p>
+              </div>
+            ) : (
+              <div className="img-with-title">
+                <img src={android} alt="android" />
+                <h4 className="d-flex">Android App Development</h4>
+              </div>
+            )}
+          </div>
+        </div>
 
-            <img src={ios} alt="ios" />
+        
+        <div className="service-content">
+          <div
+            className="col-md-6"
+            onMouseEnter={() => handleHoverEffect("iphone")}
+            onMouseLeave={handleHoverLeave}
+          >
+            {hover === "iphone" ? (
+              <div className="hoverContent">
+              <img className="imgmini" src={iosmini} alt="iosmini" />
+              <h6>Iphone App Development</h6>
+              <p>
+                We make use of modern programming languages like Swift and
+                Objective-C to offer high-end iPhone app development services.
+                With proven technologies and approaches, we have developed more
+                than 150 iPhone apps of various categories.
+              </p>
+              </div>
+            ) : (
+              <div className="img-with-title">
+                <img src={ios} alt="ios" />
+                <h4 className="d-flex">iPhone App Development</h4>
+              </div>
+            )}
           </div>
-          <div className="col-md-6">
-            <img src={ios} alt="ios" />
-          
-            <img src={ios} alt="ios" />
-          </div>
-          <div className="col-md-6">
-            <img src={ios} alt="ios" />
-         
-            <img src={ios} alt="ios" />
+          <div
+            className="col-md-6"
+            onMouseEnter={() => handleHoverEffect("android")}
+            onMouseLeave={handleHoverLeave}
+          >
+            {hover === "android" ? (
+              <div className="hoverContent">
+              <img className="imgmini" src={android} alt="androidmini" />
+              <h6>Android App Development</h6>
+              <p>
+                We have successfully developed 200+ Android apps of various categories, including on-demand, health & fitness and other top apps categories. Using Java and Kotlin, we build Android apps from scratch for Android devices to smartwatches and smart TVs.
+              </p>
+              </div>
+            ) : (
+              <div className="img-with-title">
+                <img src={android} alt="android" />
+                <h4 className="d-flex">Android App Development</h4>
+              </div>
+            )}
           </div>
         </div>
       </div>
